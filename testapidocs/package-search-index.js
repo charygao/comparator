2d7b38d6bfe14051a6802124d1e3e35c627a5031
Jenkins <jenkins@jenkins.bremersee.lokal>
2020-03-01 20:19:50 +0100

    Site checkin for project Comparator

---
+++ testapidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.bremersee.comparator"},{"l":"org.bremersee.comparator.model"},{"l":"org.bremersee.comparator.spring"},{"l":"org.bremersee.comparator.testmodel"}]
